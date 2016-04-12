fe7bf87155b868969f47aa13044f3f1403693a5a
sukanye <ray.suk@husky.neu.edu>
2016-04-12 19:07:40 -0400

    left walker function alone

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
+var WalkerArray = []
+
 // Walker constructor function
 // It will take in a position
 // It will then take a `step` every frame
-var WalkerArray = []
 var Walker = function (initialX, initialY) {
- rect(initialX,initialY,10,10)
+  
 }
 
 // This extends the Walker class. It is similar
@@ -27,8 +28,9 @@ Walker.prototype.step = function() {
 
 //
 function mouseClicked() {
-  fill(0)
-  new Walker(mouseX,mouseY, 10, 10)
+  //fill(0);
+  new Walker(mouseX,mouseY, 10, 10);
+  
   // Hint use `push` and `new` to make instances of walkers
   // You might want to pass in mouseX and mouseY
 
