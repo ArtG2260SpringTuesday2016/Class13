1bf81f756291e13c15501e1cc1cf6227010e480f
Mnshuster <shuster.mc@husky.neu.edu>
2016-04-12 21:16:42 -0400

    it worked!

--- sketch.js
+++ sketch.js
@@ -3,7 +3,20 @@
 // It will then take a `step` every frame
 var walkerArray = []
 var Walker = function (initialX, initialY) {
- rect(initialX,initialY,10,10)
+  
+
+
+
+  this.x = initialX;
+  this.y = initialY;
+ this.width = 5;
+  this.height = 5;
+
+
+ this.renderRect = function() {
+    fill('black');
+    rect(this.x, this.y, this.width, this.height)
+  }
 }
 
 // This extends the Walker class. It is similar
@@ -28,7 +41,7 @@ Walker.prototype.step = function() {
 //
 function mouseClicked() {
   fill(0)
-  new Walker(mouseX,mouseY)
+  walkerArray.push(new Walker(mouseX,mouseY));
   // Hint use `push` and `new` to make instances of walkers
   // You might want to pass in mouseX and mouseY
 
@@ -36,10 +49,11 @@ function mouseClicked() {
 
 // Global scope for an array of walkers
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(windowWidth,windowHeight-30)
   background(255)
+  console.log(walkerArray);
   
-  walkerArray.push(new Walker(mouseX,mouseY));
+  
 
   // Set up some kind of background
   // createCanvas with `windowWidth` and
@@ -49,15 +63,13 @@ function setup() {
 
 
 function draw() {
-  // Tell every walker to take a step
-
+ 
+ var youwillwalkdammit = function(walkerArray){
+  for(i = 0; i < walkerArray.length; i++){
+   walkerArray[i].step();
+   walkerArray[i].renderRect();
+ 
+ }
 }
-
-
-/*
-  When you have this working try implementing
-  some of the following:
-    - color or shape changes on key press
-    - garbage collection, if a Walker moves off screen, kill it.
-    - something we come up with in class
-*/
+youwillwalkdammit(walkerArray)
+}
