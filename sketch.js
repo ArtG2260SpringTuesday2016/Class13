ec15553e41a6139c7d48646cf2097415cc9d70be
MistaFish31 <zhang.ma@husky.neu.edu>
2016-04-12 20:42:56 -0400

    rectangles move upward...

--- sketch.js
+++ sketch.js
@@ -1,50 +1,52 @@
 var walkers = [];
+var spider;
 
 var Walker = function (initialX, initialY) {
   this.x=initialX;
-  this.y=initialY
-}
+  this.y=initialY;
 
-Walker.prototype.step = function() {
-  var choice = Math.floor(random(0,3))
-
-  if (choice === 0) {
-    this.x++;
-  }
-  if (choice === 1) {
-    this.x--;
-  }
-  if (choice === 2) {
-    this.y++;
-  }
-  else {
-    this.y--;
-  }
-  
-  this.render{
+  this.display=function(){
     rect(this.x,this.y,5,5)
   }
+
+  this.move = function() {
+    var choice = Math.floor(random(0,3))
+  
+    if (choice === 0) {
+      this.x++;
+    }
+    if (choice === 1) {
+      this.x--;
+    }
+    if (choice === 2) {
+      this.y++;
+    }
+    else {
+      this.y--;
+    }
+  }
+  
+  this.render=function(){
+    this.display();
+    this.move();
+  }
 }
 
-var spider = new Walker(this.x,this.y)
-
 function mouseClicked() {
-  spider.push(new Walker(mouseX,mouseY))
+  new Walker(mouseX,mouseY)
+  walkers.push(new Walker(mouseX,mouseY))
 }
 
 // Global scope for an array of walkers
 function setup() {
   color(175,175,175)
   createCanvas(windowWidth,windowHeight)
-  spider=new Walker
 }
 
 function draw() {
-  Walker.prototype.step()
-  Walker.prototype.render()
-  console.log(spider.x,spider.y)
-  
-
+  for(var i=0; i<walkers.length;i++){
+    walkers[i].render()
+  }
 }
 
 
