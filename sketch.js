681e38086589345f75f9d0220c218f2033ea97d4
sukanye <ray.suk@husky.neu.edu>
2016-04-12 21:03:33 -0400

    things stopped moving

--- sketch.js
+++ sketch.js
@@ -1,58 +1,64 @@
-var WalkerArray = []
+var walkers = [];
 
 // Walker constructor function
 // It will take in a position
 // It will then take a `step` every frame
 var Walker = function (initialX, initialY) {
+  this.x = initialX;
+  this.y = initialY;
+  this.width = 5;
+  this.height = 5;
   
+  this.renderRect = function() {
+    fill('black');
+    rect(this.x, this.y, this.width, this.height)
+  }
 }
+
 
 // This extends the Walker class. It is similar
 // to just including it inside of the Walker constructor function.
 Walker.prototype.step = function() {
-  var choice = Math.floor(random(0,3))
+  var choice = random(0, 4)
 
-  if (choice === 0) {
+  if (choice===0) {
     this.x++;
   }
-  if (choice === 1) {
+  if (choice===1) {
     this.x--;
   }
-  if (choice === 2) {
+  if (choice===2) {
     this.y++;
   }
-  else {
+  else if(choice===3) {
     this.y--;
   }
-}
+};
 
-//
+
 function mouseClicked() {
-  //fill(0);
-  new Walker(mouseX,mouseY, 10, 10);
-  
-  // Hint use `push` and `new` to make instances of walkers
-  // You might want to pass in mouseX and mouseY
+  walkers.push(new Walker(mouseX, mouseY));
+  walkers[walkers.length] = new Walker(mouseX, mouseY)
 
 }
 
 // Global scope for an array of walkers
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  background(255)
-
   // Set up some kind of background
   // createCanvas with `windowWidth` and
   // `windowHeight` minus height of title
-
+  createCanvas(windowWidth, windowHeight - 32);
+  background('White');
+console.log(walkers);
 }
 
 function draw() {
   // Tell every walker to take a step
-
+    for(i = 0; i < walkers.length; i++){
+      walkers[i].step()
+      walkers[i].renderRect()
+    }
 }
-
-
 /*
   When you have this working try implementing
   some of the following:
@@ -60,3 +66,5 @@ function draw() {
     - garbage collection, if a Walker moves off screen, kill it.
     - something we come up with in class
 */
+
+
